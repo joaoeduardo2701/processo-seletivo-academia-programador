99f6c02db067bedab1bd6138086056f16cc4d8a8
Eduardo Cardoso <63940232+joaoeduardo2701@users.noreply.github.com>
2024-02-27 13:54:07 -0300

    Update teste01.py

--- Teste 01 - Python/teste01.py
+++ Teste 01 - Python/teste01.py
@@ -2,10 +2,6 @@ lado1 = float(input("Digite o valor 1º lado: "))
 lado2 = float(input("Digite o valor 2º lado: "))
 lado3 = float(input("Digite o valor 3º lado: "))
 
-angulo1 = int(input("Digite o valor do 1º ângulo: "))
-angulo2 = int(input("Digite o valor do 2º ângulo: "))
-angulo3 = int(input("Digite o valor do 3º ângulo: "))
-
 if(lado1 + lado2 < lado3 or lado2 + lado3 < lado1 or lado1 + lado1 + lado3 < lado2):
     print("Triângulo inválido!")
 elif(lado1 != lado2 and lado2 != lado3 and lado3 != lado1):
